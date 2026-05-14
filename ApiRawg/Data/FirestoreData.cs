04fe86cae3ec25c3f1d562096f6424725eb3d362
Nicolas de Oliveira <nicolas.lima2108@gmail.com>
2026-05-13 21:21:32 -0300

    Adicionar arquivos de projeto.

---
+++ ApiRawg/Data/FirestoreData.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+
+namespace ApiRawg.Data
+{
+    public class FirestoreData
+    {
+
+        public FirestoreData Db { get; private set;}
+
+        public FirestoreData()
+        {
+           
+        }
+
+    }
+}
---
+++ ApiRawg/Models/Jogo.cs
@@ -0,0 +1,25 @@
+﻿using Google.Cloud.Firestore;
+
+namespace ApiRawg.Models
+{
+    [FirestoreData]
+    public class Jogo
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+        [FirestoreProperty] 
+        public string Nome { get; set; }
+        [FirestoreProperty]
+        public string Descricao { get; set; }
+        [FirestoreProperty]
+        public string ImagemUrl { get; set; }
+        [FirestoreProperty]
+        public double Avaliacao { get; set; }
+        [FirestoreProperty]
+        public int Classificacao { get; set; }
+        [FirestoreProperty]
+        public DateTime Upload { get; set; }
+
+
+    }
+}
